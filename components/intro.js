f3ae776be3374c9d04c17bb85fc51a521aa2ce9c
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2021-04-22 11:06:28 +0200

    Gradient title

--- components/intro.js
+++ components/intro.js
@@ -5,7 +5,7 @@ export default function Intro({
 
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8 bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500">
         {title}
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
