c5cb675f7f15f5b14e3a16195be7c6cc6591ad9b
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2021-04-29 10:40:43 +0200

    Fix page width on mobile devices

--- components/post-body.js
+++ components/post-body.js
@@ -12,13 +12,21 @@ export default function PostBody({ content }) {
           renderBlock={({ record }) => {
             switch (record.__typename) {
               case "ImageBlockRecord":
-                return <ResponsiveImage img={record.image.responsiveImage} />;
+                return <ResponsiveImage
+                  img={record.image.responsiveImage}
+                  className="max-w-max mx-auto"
+                />;
               case "VideoBlockRecord":
-                return <ReactPlayer url={record.video.url} controls="true" />
+                return <ReactPlayer
+                  url={record.video.url}
+                  controls="true"
+                  className="max-w-max mx-auto"
+                />
               case "SoundcloudBlockRecord":
                 return <ReactPlayer
                   url={record.url}
                   controls="true"
+                  className="max-w-max mx-auto"
                 />
 
               default:
