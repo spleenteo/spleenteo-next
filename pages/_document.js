ad053efc56550c92e938382c4832fb6bfee6180b
Matteo Papadopoulos <m.papadopoulos@cantierecreativo.net>
2021-09-04 12:16:36 +0200

    Add analytics

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,20 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
-        <Head />
+      <Html lang="it">
+        <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y992JCKGDQ"></script>
+          <script
+            async
+            dangerouslySetInnerHTML={{
+              __html: `window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-Y992JCKGDQ');`
+            }}
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
